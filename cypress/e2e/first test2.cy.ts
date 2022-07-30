d4dd3cab8516a50b04d90a923cf2678c4be36a1f
Gul <livello@Livellos-MacBook-Pro.local>
2022-07-30 13:24:21 +0200

    Click sorting icon

--- cypress/e2e/first test2.cy.ts
+++ cypress/e2e/first test2.cy.ts
@@ -35,12 +35,7 @@ describe("🚀 Visit Page and Login",() =>{
         cy.scrollTo('top')
         .get("div[id='inventory_container']")
         .contains("Sauce Labs Bike Light")
-<<<<<<< HEAD
-        .click()
-        .get("buttom[id='add-to-cart-sauce-labs-bike-light']")
-=======
         .get("button[id='add-to-cart-sauce-labs-bike-light']")
->>>>>>> 5a38e97d1a1c3f2944754da23abe233c939e39cd
     })
 
     it("Select Another Item", () => {
@@ -48,6 +43,12 @@ describe("🚀 Visit Page and Login",() =>{
         .get("button[name='add-to-cart-sauce-labs-bolt-t-shirt']")
         .click()
     })  
+
+    it("Click Sorting Field", () => {
+        cy
+        .get("select[data-test='product_sort_container']")
+        .select('Price (low to high)')
+    })
 
     
 })
